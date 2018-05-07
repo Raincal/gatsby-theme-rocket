7b80da3e67dbef53e469cf6525db8296156649df
Raincal <cyj94228@gmail.com>
2018-05-07 23:57:30 +0800

    fix(header): fix about link

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -26,7 +26,7 @@ const Header = ({ title, showMask, toggle }) => {
       <h1>
         <Link to="/">{title}</Link>
       </h1>
-      <Link className={styles.me} to="/about" />
+      <Link className={styles.about} to="/about/" />
     </header>
   )
 }
