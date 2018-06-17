039e079b9c53ac0050f80ef956bc4af95bdaffe8
Raincal <cyj94228@gmail.com>
2018-06-18 07:32:52 +0800

    Update config

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,9 @@
+const siteUrl = 'https://blog.raincal.com'
+
 module.exports = {
+  siteUrl,
   title: "Raincal's Blog",
   description: 'Web developer/React/Node.js',
-  siteUrl: 'https://blog.raincal.com',
   navButtons: [
     { name: '首页', icon: 'home', url: '/' },
     { name: '归档', icon: 'archive', url: '/archives/' },
@@ -11,7 +13,7 @@ module.exports = {
   ],
   socialLinks: [
     { name: 'GitHub', icon: 'github-v', url: 'https://github.com/Raincal' },
-    { name: 'RSS', icon: 'rss-v', url: '/rss.xml' },
+    { name: 'RSS', icon: 'rss-v', url: `${siteUrl}/rss.xml` },
     {
       name: 'Search',
       icon: 'search',
