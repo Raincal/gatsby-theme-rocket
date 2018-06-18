d00e2444392311ba42817d94ea41c6b9c6874e79
Raincal <cyj94228@gmail.com>
2018-06-18 08:51:26 +0800

    refactor: use short syntax for React.Fragment

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import Post from './Post'
 
-const Posts = ({ posts }) => {
-  const Posts = posts.map(({ node }) => (
-    <Post key={node.id} post={node} isHome />
-  ))
-
-  return <React.Fragment>{Posts}</React.Fragment>
-}
+const Posts = ({ posts }) => (
+  <>{posts.map(({ node }) => <Post key={node.id} post={node} isHome />)}</>
+)
 
 export default Posts
