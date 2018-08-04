b09984ef7dd6ee1a99b21ccd18fad36747b95a1b
Raincal <cyj94228@gmail.com>
2018-08-05 05:50:02 +0800

    :recycle: (avatar) Using background image as avatar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,7 +2,6 @@ import classNames from 'classnames/bind'
 import { Link, navigateTo } from 'gatsby'
 import React from 'react'
 import styles from 'styles/header.module.css'
-import avatar from '../../images/avatar.png'
 
 let cx = classNames.bind(styles)
 
@@ -26,12 +25,7 @@ const Header = ({ title, showMask, toggle }) => (
     <h1>
       <Link to="/">{title}</Link>
     </h1>
-    <img
-      src={avatar}
-      alt="avatar"
-      className={styles.about}
-      onClick={() => navigateTo('/about/')}
-    />
+    <div className={styles.about} onClick={() => navigateTo('/about/')} />
   </header>
 )
 
--- src/components/Sidebar/Profile.jsx
+++ src/components/Sidebar/Profile.jsx
@@ -1,16 +1,10 @@
 import { navigateTo } from 'gatsby'
 import React from 'react'
 import styles from 'styles/profile.module.css'
-import avatar from '../../images/avatar.png'
 
 const Profile = ({ title }) => (
   <div className={styles.profile}>
-    <img
-      src={avatar}
-      alt="avatar"
-      className={styles.avatar}
-      onClick={() => navigateTo('/')}
-    />
+    <div className={styles.avatar} onClick={() => navigateTo('/')} />
     <span className={styles.title}>{title}</span>
   </div>
 )
