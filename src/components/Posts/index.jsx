586da5ed6f4a2af4274cc91e5625185b084e2b2c
Raincal <cyj94228@gmail.com>
2018-08-05 05:55:08 +0800

    :art: Format code

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import Post from './Post'
 
 const Posts = ({ posts }) => (
-  <>{posts.map(({ node }) => <Post key={node.id} post={node} isHome />)}</>
+  <>
+    {posts.map(({ node }) => (
+      <Post key={node.id} post={node} isHome />
+    ))}
+  </>
 )
 
 export default Posts
