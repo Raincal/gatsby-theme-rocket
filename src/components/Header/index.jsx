3d18c45d37fd4da7b4a1c342013816d6e2e618da
Raincal <cyj94228@gmail.com>
2019-01-25 22:23:45 +0800

    :zap: (avatar) Using cdn

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames/bind'
-import { Link, navigate } from 'gatsby'
+import { Link } from 'gatsby'
 import React from 'react'
 import styles from 'styles/header.module.css'
 
@@ -25,7 +25,13 @@ const Header = ({ title, showMask, toggle }) => (
     <h1>
       <Link to="/">{title}</Link>
     </h1>
-    <div className={styles.about} onClick={() => navigate('/about/')} />
+    <Link to="/about/">
+      <img
+        src='https://cdn.yuque.com/yuque/0/2018/png/123526/1525979671576-avatar/32b284df-3354-4e03-ab59-fbb06f71e78e.png?x-oss-process=image/resize,m_fill,w_140,h_140'
+        alt="avatar"
+        className={styles.about}
+      />
+    </Link>
   </header>
 )
 
--- src/components/Sidebar/Profile.jsx
+++ src/components/Sidebar/Profile.jsx
@@ -1,10 +1,17 @@
-import { navigate } from 'gatsby'
+import { Link } from 'gatsby'
 import React from 'react'
 import styles from 'styles/profile.module.css'
 
 const Profile = ({ title }) => (
   <div className={styles.profile}>
-    <div className={styles.avatar} onClick={() => navigate('/')} />
+    <Link to="/">
+      <img
+        src='https://cdn.yuque.com/yuque/0/2018/png/123526/1525979671576-avatar/32b284df-3354-4e03-ab59-fbb06f71e78e.png?x-oss-process=image/resize,m_fill,w_140,h_140'
+        alt="avatar"
+        className={styles.avatar}
+      />
+    </Link>
+    
     <span className={styles.title}>{title}</span>
   </div>
 )
