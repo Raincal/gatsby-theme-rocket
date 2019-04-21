d3c377288ed5c1103e21c0a2a5879dddaa70115a
Raincal <cyj94228@gmail.com>
2019-04-21 23:53:59 +0800

    :zap: (home) remove js and json links

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,11 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+exports.onPreRenderHTML = ({ pathname, getHeadComponents, replaceHeadComponents, replacePostBodyComponents }) => {
+  const regex = new RegExp(/\.(js|json)$/)
+  let headComponents = getHeadComponents()
 
-// You can delete this file if you're not using it
+  if (pathname === '/') {
+    headComponents = headComponents.filter(comp => !regex.test(comp.key))
+
+    replaceHeadComponents(headComponents)
+    replacePostBodyComponents([])
+  }
+}
