7db02985fb97c718b475549b817804f2cbe42018
Raincal <cyj94228@gmail.com>
2019-05-21 10:21:19 +0800

    Update avatar

--- src/components/Sidebar/Profile.jsx
+++ src/components/Sidebar/Profile.jsx
@@ -6,7 +6,7 @@ const Profile = ({ title }) => (
   <div className={styles.profile}>
     <Link to="/">
       <img
-        src='https://cdn.yuque.com/yuque/0/2018/png/123526/1525979671576-avatar/32b284df-3354-4e03-ab59-fbb06f71e78e.png?x-oss-process=image/resize,m_fill,w_140,h_140'
+        src='https://ae01.alicdn.com/kf/HTB1Vu.RXBSD3KVjSZFq7634bpXaf.png'
         alt="avatar"
         className={styles.avatar}
       />
