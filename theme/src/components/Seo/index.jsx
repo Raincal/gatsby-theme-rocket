d417e24cc1596d17b1b17c5e3ba60f9b9400fd2f
Raincal <cyj94228@gmail.com>
2020-05-08 09:46:30 +0800

    :bug: fix react-helmet

--- theme/src/components/Seo/index.jsx
+++ theme/src/components/Seo/index.jsx
@@ -1,6 +1,6 @@
 import { graphql, StaticQuery } from 'gatsby'
 import React from 'react'
-import Helmet from 'react-helmet'
+import { Helmet } from 'react-helmet'
 
 const SEO = ({ description, lang, meta, keywords, title }) => (
   <StaticQuery
